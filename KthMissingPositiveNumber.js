62cae52576c95371c7b51a1c4692d051d0829f92
Nahuel <91757112+M-Nahuel@users.noreply.github.com>
2023-03-06 13:48:35 -0300

    all testcases fixed

--- KthMissingPositiveNumber.js
+++ KthMissingPositiveNumber.js
@@ -3,12 +3,12 @@
  * @param {number} k
  * @return {number}
  */
-function list(array){
+function list(array,n){
     let ls = [];
-    for(let i=0;i<array[array.length-1];i++){
+    for(let i=0;i<array[array.length-1]+n;i++){
         ls.push(i+1);
     };
-    console.log(ls)
+    
     return ls;
 }
 
@@ -23,16 +23,11 @@ function miss(input,ordered){
 }
 
 var findKthPositive = function(arr, k) {
-    let ls = list(arr);
+    let ls = list(arr,k);
     let missing = miss(arr,ls);
-    if(missing.length<1 || missing[missing.length-1]<ls[ls.length-1]){
-        for(let i=1; i<k+3;i++){
-            ls.push(k+i);
-        };
-        missing = miss(arr,ls);
-    }
-    console.log(missing);
+
     return missing[k-1];
 };
 
+console.log(findKthPositive([1,3], 5));
 console.log(findKthPositive([5,6,7,8,9], 9));
