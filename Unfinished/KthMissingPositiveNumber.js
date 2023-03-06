a8f05498f15b6bc7b0a6bb2225e7cefe8f8dbfac
Nahuel <91757112+M-Nahuel@users.noreply.github.com>
2023-03-06 11:56:36 -0300

    Update KthMissingPositiveNumber.js
    now the program can support the case where: array = [1,2,3,4], k = 2
    toDo: improve to support the case where array = [5,6,7,8,9], k = 9

--- Unfinished/KthMissingPositiveNumber.js
+++ Unfinished/KthMissingPositiveNumber.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 function list(array){
-    ls = [];
+    let ls = [];
     for(let i=1;i<array[array.length-1];i++){
         ls.push(i);
     };
@@ -25,10 +25,11 @@ var findKthPositive = function(arr, k) {
     let ls = list(arr);
     let missing = miss(arr,ls);
     if(missing.length<1){
-        for(let i=1; i<k+2;i++){
+        for(let i=1; i<k+3;i++){
             ls.push(k+i);
         };
         missing = miss(arr,ls);
     }
+    console.log(missing);
     return missing[k-1];
 };
